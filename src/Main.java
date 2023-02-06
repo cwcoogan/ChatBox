c917021e66513611c2eec741e005fc9ca2513bb2
chase coogan <chasecoogan12@gmail.com>
2023-02-06 02:32:12 -0500

    file restructure

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ package src;
 
 public class Main {
   public static void main(String[] args) {
-    UserTest t = new UserTest("ff", "gg");
+    User t = new User("ff", "gg");
 
     System.out.println(t.toString());
   }
--- src/User.java
+++ src/User.java
@@ -1,44 +1,51 @@
-//package src;
-//
-//import org.w3c.dom.ls.LSOutput;
-//
-//import java.util.ArrayList;
-//import java.util.Scanner;
-//
-//public class User implements IUser{
-//  private String name;
-//  private String pwd;
-//  Scanner scanner = new Scanner(System.in);
-//
-//
-//  public User(String name, String pwd) {
-//    this.name = name;
-//    this.pwd = pwd;
-//  }
-//
-//  public String getName() {
-//    return name;
-//  }
-//
-//  public String getPwd() {
-//    return pwd;
-//  }
-//
-//
-//
-//
-////  public void login(String userName) {
-////    if getName() instanceof //an array
-//
-//
-//  }
-//
-//
-////   create userNames
-//  public String createUserName(Scanner scanner) {
-//    System.out.println("Create a username: ");
-//    return scanner.nextLine();
-//  }
-//
-//
-//}
+package src;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User {
+  String userName;
+  String pwd;
+
+
+  public User(String userName, String pwd) {
+    this.userName = userName;
+    this.pwd = pwd;
+
+  }
+
+  // create userName
+  public static String createUserName() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Create a username: ");
+    return scanner.nextLine();
+  }
+
+
+  // create ArrayList for userNames
+  public static ArrayList<String> storeUsernamesInList() {
+    ArrayList<String> usernameList = new ArrayList<>();
+    System.out.println("Welcome. Enter your username: ");
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+
+    System.out.println("Error. user not found.");
+    System.out.println("Create new user?: ");
+    Scanner scan2 = new Scanner(System.in);
+    String input2 = scan.nextLine();
+    if (input2 == "yes") {
+      System.out.println("Enter new username: ");
+      Scanner scan3 = new Scanner(System.in);
+      String input3 = scan.nextLine();
+    }
+    usernameList.add(createUserName());
+    return usernameList;
+  }
+
+
+    // to see the string representation.
+    @Override
+    public String toString() {
+      return "users: " + storeUsernamesInList();
+    }
+  }
